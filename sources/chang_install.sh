0b57c1506834ed17b30972dc6e2493b322ae3668
majkel <m@majkel.cc>
2018-12-04 21:15:27 +0100

    Remove CHANG_HOME

--- sources/chang_install.sh
+++
@@ -1,8 +0,0 @@
-chang_install() {
-  mkdir -p ${CHANG_HOME}
-  rm -f ${CHANG_HOME}/"${CHANG_APP_NAME}"
-  ln -s "${CHANG_PWD}" ${CHANG_HOME}/"${CHANG_APP_NAME}"
-  if [[ ${CHANG_REV_PROXY_ENABLED} ]]; then
-    hal-rev-proxy install $CHANG_APP_NAME $CHANG_APP_PORT
-  fi
-}
--- sources/chang_rev_proxy_start.sh
+++ sources/chang_rev_proxy_start.sh
@@ -1,16 +1,7 @@
 chang_rev_proxy_start() {
-  networks=""
-  if [ -d $CHANG_HOME ]; then
-    for app in $(ls -1 $CHANG_HOME); do
-      chang_create_network $(chang_app_network $app)
-      networks="${networks} --network=$(chang_app_network $app)"
-    done
-  fi
-
   docker run -d \
     --name ${CHANG_REV_PROXY_CONTAINER} \
     -p ${CHANG_REV_PROXY_BIND}:${CHANG_REV_PROXY_PORT}:80 \
     -p ${CHANG_REV_PROXY_BIND}:443:443 \
-    $networks \
     $CHANG_REV_PROXY_IMAGE > /dev/null
 }
