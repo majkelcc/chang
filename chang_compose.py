d663d527e4a55176d791e8e653ee29d45ef5ff8d
majkel <m@majkel.cc>
2019-01-09 20:06:17 +0800

    Correctly generate compose file when CHANG_SYNC_ENABLED is false

--- chang_compose.py
+++ chang_compose.py
@@ -13,6 +13,8 @@ CHANG_APP_ID = os.environ["CHANG_APP_ID"]
 CHANG_APP_NAME = os.environ["CHANG_APP_NAME"]
 CHANG_NETWORK = os.environ["CHANG_NETWORK"]
 CHANG_SET = os.environ["CHANG_SET"]
+CHANG_SYNC_ENABLED = (os.environ["CHANG_SYNC_ENABLED"] == "true")
+
 
 if len(sys.argv) != 2:
   chang_error("USAGE...")
@@ -62,7 +64,11 @@ for name in chang_compose["services"]:
     volume_mount = match.group(2)
     if not service.get("volumes", False):
       service["volumes"] = []
-    service["volumes"].append(VOLUMES_MAPPER[volume_name] + ":" + volume_mount)
+    if CHANG_SYNC_ENABLED or volume_name != "chang":
+      volume_key = VOLUMES_MAPPER[volume_name]
+    else:
+      volume_key = "./"
+    service["volumes"].append(volume_key + ":" + volume_mount)
   env = service.get("environment", [])
   service["environment"] = environment + env
   service["networks"] = { "chang": { "aliases": [chang_service_network_alias(name)] }}
@@ -92,7 +98,8 @@ yaml.dump(compose_yaml, compose_file, default_flow_style=False)
 
 volumes_file = open("{0}/volumes".format(CHANG_TMP_PATH), "w")
 for volume in VOLUMES_MAPPER.map:
-  volumes_file.write(chang_external_volume_name(volume) + "\n")
+  if CHANG_SYNC_ENABLED or volume_name != "chang":
+    volumes_file.write(chang_external_volume_name(volume) + "\n")
 
 proxy_file = open("{0}/proxy".format(CHANG_TMP_PATH), "w")
 if chang_compose.get("server", {}).get("root", False):
