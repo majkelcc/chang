f7f69a628ea73f1ca4a3ec197e904d7597255ff0
majkel <m@majkel.cc>
2019-07-29 14:30:19 +0200

    Run `chang_compose ps` when no arguments are passed

--- sources/chang_run_command.sh
+++ sources/chang_run_command.sh
@@ -1,4 +1,9 @@
 chang_run_command() {
+  if [[ $# -eq 0 ]]; then
+    chang_compose ps
+    return $?
+  fi
+
   command=($(chang_find_command "$@"))
   if [[ $CHANG_DEBUG == true ]]; then
     set -x
