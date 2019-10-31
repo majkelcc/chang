6f9795b0d14e2fc78bc8fa5244094de50782e358
majkel <m@majkel.cc>
2019-10-31 15:46:14 +0000

    Fix escaping arguments

--- sources/chang_find_command.sh
+++ sources/chang_find_command.sh
@@ -1,16 +1,20 @@
 chang_find_command() {
   command_dir=".chang/bin"
 
+  local level=0
+
   while [[ ! -z ${1:-} ]]; do
     test -d $command_dir/$1 || break
     command_dir+="/$1"
     shift
+    ((level++))
   done
 
   if [[ ! -z ${1:-} && -f $command_dir/$1 ]]; then
-    echo "$command_dir/$1" "${@:2}"
+    ((level++))
+    echo "$command_dir/$1" $level
   elif [[ -f $command_dir/${command_dir##*/} ]]; then
-    echo "$command_dir/${command_dir##*/}" "${@:-}"
+    echo "$command_dir/${command_dir##*/}" $level
   else
     chang_error "Command not found in $command_dir/${@:-}"
   fi
--- sources/chang_run_command.sh
+++ sources/chang_run_command.sh
@@ -8,5 +8,5 @@ chang_run_command() {
   if [[ $CHANG_DEBUG == true ]]; then
     set -x
   fi
-  bash $CHANG_SET $command "${command[@]:1}" || chang_command_error $? "$@"
+  bash $CHANG_SET $command "${@:$((${command[1]} + 1))}" || chang_command_error $? "$@"
 }
