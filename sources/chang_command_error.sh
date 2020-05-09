4b09ea81cc42511847ae34f9e6118bce84dde3d8
majkel <m@majkel.cc>
2020-05-09 20:18:43 +0700

    Better errors

--- sources/chang_command_error.sh
+++ sources/chang_command_error.sh
@@ -1,4 +1,4 @@
 chang_command_error() {
-  printf "$(chang_color_cmd_error)chang ${@:2} returned $1$(chang_color_reset)\n" >&2
+  printf "$(chang_color_cmd_error)🔥 chang ${@:2} returned $1$(chang_color_reset) 🔥\n" >&2
   exit 1
 }
