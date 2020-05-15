7f8c9c81517f1da101cbe371ff4847c54d7029b5
majkel <m@majkel.cc>
2020-05-15 23:43:20 +0700

    Quote

--- sources/chang_compose_init.sh
+++ sources/chang_compose_init.sh
@@ -11,9 +11,8 @@ chang_compose_init() {
     local branch_marker_file=$CHANG_TMP_PATH/last_used_branch
     touch $branch_marker_file
 
-    local current_branch=$(chang_current_branch)
     local last_used_branch=$(cat $branch_marker_file)
-    if ! chang_compare_commit .chang || [[ $current_branch != $last_used_branch ]]; then
+    if ! chang_compare_commit .chang || [[ $CHANG_BRANCH != $last_used_branch ]]; then
       (
         if [[ -f $DOCKER_COMPOSE_FILE ]]; then
           export COMPOSE_PROJECT_NAME=$(chang_app_id $CHANG_APP_NAME $CHANG_APP_HASH)${last_used_branch:+_$(chang_app_hash $last_used_branch)}
@@ -24,7 +23,7 @@ chang_compose_init() {
       chang_reload
       chang_init
 
-      printf $current_branch > $branch_marker_file
+      printf "$CHANG_BRANCH" > $branch_marker_file
       chang_update_commit .chang
     fi
   fi
