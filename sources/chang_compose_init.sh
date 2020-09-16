e98460e71d1c30e2b0f562f9419eb38c15e308ea
majkel <m@majkel.cc>
2020-09-17 01:19:05 +0700

    Reload also on branch change

--- sources/chang_compose_init.sh
+++ sources/chang_compose_init.sh
@@ -1,18 +1,18 @@
 chang_compose_init() {
   if chang_memo CHANG_COMPOSE_INIT; then
+    last_branch_file=$CHANG_TMP_PATH/last_branch
+    touch $last_branch_file
+    last_branch=$(cat $last_branch_file)
+
     if [[ $CHANG_SYNC_ENABLED == true ]]; then
       chang-sync >/dev/null
     fi
 
-    if ! chang_compare_commit .chang; then
+    if ! chang_compare_commit .chang || [[ $CHANG_BRANCH != $last_branch ]]; then
       chang_reload
-      chang_init
       chang_update_commit .chang
     fi
 
-    last_branch_file=$CHANG_TMP_PATH/last_branch
-    touch $last_branch_file
-    last_branch=$(cat $last_branch_file)
     if [[ $CHANG_BRANCH != $last_branch ]]; then
       local running_containers=$(docker ps | grep -oE ${CHANG_APP_ID}_'[^[:space:]]*' | grep -v $CHANG_BRANCH_HASH)
       if [[ ! -z $running_containers ]]; then
@@ -20,6 +20,11 @@ chang_compose_init() {
         docker kill $running_containers
       fi
       printf "$CHANG_BRANCH" > $last_branch_file
+      chang_reload
+    fi
+
+    if [[ $CHANG_REV_PROXY_ENABLED == true ]]; then
+      chang_init
     fi
   fi
 }
