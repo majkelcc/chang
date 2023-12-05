537be2fb6c8ff4d25979f83eba39d2f1ec6ff15e
majkel <m@majkel.cc>
2023-12-05 17:55:39 +0100

    fix(notice): stderr

--- sources/chang_notice.sh
+++ sources/chang_notice.sh
@@ -1,6 +1,6 @@
 chang_notice() {
-  printf "\033[1K"
-  printf "\r"
+  printf "\033[1K" >&2
+  printf "\r" >&2
   printf "$(chang_color_notice)▷ " >&2
   echo -n "${@}" >&2
   printf "$(chang_color_reset)\n" >&2
